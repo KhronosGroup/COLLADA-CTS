cfe561ebce905f309da19b6a304a3a38d32398d9
Greg Corson <greg_corson@playstation.sony.com>
2007-07-25 00:08:25 +0000

    Blessed images of seymourplane have been updated judging scripts added to seymour animations, these show how to structure an intermediate judging script so for the "basic" badge, only not crashing is required.

---
+++ StandardDataSets/aggregate/seymour_polylist_animation/seymour_polylist_animation.py
@@ -0,0 +1,54 @@
+# Copyright (C) 2007 Khronos Group
+# Available only to Khronos members.
+# Distribution of this file or its content is strictly prohibited.
+
+# See Core.Logic.FJudgementContext for the information
+# of the 'context' parameter.
+# [WARNING] this structure is subject to changes.
+#
+
+# This sample judging object does the following:
+#
+# JudgeBasic: just verifies that the standard steps did not crash.
+# JudgeIntermediate: also verifies that the validation steps are not in error.
+# JudgeAdvanced: same as intermediate badge.
+
+class SimpleJudgingObject:
+    def __init__(self):
+        pass
+        
+    def JudgeBasic(self, context):
+            
+        # This is where you can test XML or force the comparison of image files
+        # or any custom verification you want to do...
+        if (context.HasStepCrashed()):
+            context.Log("FAILED: Crashes during required steps.")
+            return False
+        else:
+            context.Log("PASSED: No crashes.")
+            return True
+      
+    # To pass intermediate you need to pass basic, this object could also include additional 
+    # tests that were specific to the intermediate badge.
+    # QUESTION: Is there a way to fetch the result of JudgeBasic so we don't have to run it again?
+    def JudgeIntermediate(self, context):
+        if not self.JudgeBasic(context):
+            return False
+            
+         # Check the required steps for positive results.
+        if not context.HaveStepsPassed([ "Import", "Export", "Render", "Validate" ]):
+            context.Log("FAILED: Import, export and validate steps must be present and successful.")
+            return False
+                    
+        context.Log("PASSED: Required steps executed and passed.")
+        return True
+            
+    # To pass advanced you need to pass intermediate, this object could also include additional
+    # tests that were specific to the advanced badge
+    def JudgeAdvanced(self, context):
+        return self.JudgeIntermediate(context)
+       
+# This is where all the work occurs: "judgingObject" is an absolutely necessary token.
+# The dynamic loader looks very specifically for a class instance named "judgingObject".
+#
+judgingObject = SimpleJudgingObject();
