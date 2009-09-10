8586cf78d7097ea0ee405d71d8c834702fe4306a
tswong <tswong@example.com>
2009-09-10 21:25:18 +0000

    Tony Wong: Committing changes and additions to library_animations test cases

--- StandardDataSets/collada/library_animations/animation/sampler/StepBezier/StepBezier.py
+++ StandardDataSets/collada/library_animations/animation/sampler/StepBezier/StepBezier.py
@@ -4,169 +4,69 @@
 
 # See Core.Logic.FJudgementContext for the information
 # of the 'context' parameter.
-# [WARNING] this structure is subject to changes.
-#
 
 # This sample judging object does the following:
 #
 # JudgeBaseline: just verifies that the standard steps did not crash.
-# JudgeExemplary: also verifies that the validation steps are not in error.
-# JudgeSuperior: same as intermediate badge.
+# JudgeSuperior: also verifies that the validation steps are not in error.
+# JudgeExemplary: same as intermediate badge.
 
-# Status value:
-# -1000 : Initail status: test does not start yet.
-# 0 : Failed
-# 1 : Successful
-
-from Core.Common.DOMParser import *
-from Core.Common.CheckingModule import *
-
-from xml.dom.minidom import parse, parseString
-import os
+# We import an assistant script that includes the common verifications
+# methods. The assistant buffers its checks, so that running them again
+# does not incurs an unnecessary performance hint.
+from StandardDataSets.scripts import JudgeAssistant
 
 # Please feed your node list here:
-tagLst = ['library_animations', 'animation', 'source']
+tagLst = ['library_animations', 'animation', 'source', 'Name_array']
+attrName = ''
+attrVal = ''
+dataToCheck = ''
 
 class SimpleJudgingObject:
-    def __init__(self, status_basic_, status_intermediate_, status_advanced_, tagLst_):
-        self.status_basic = status_basic_
-        self.status_intermediate = status_intermediate_
-        self.status_advanced = status_advanced_
-        self.tagLst = tagLst_
-        self.msgB2M = ''
-        self.inputFilleName = ''
-        self.outputFilleNameLst = []
-        
-    def NameArrayChecking(self):
-        
-        testIO = DOMParserIO( self.inputFilleName, self.outputFilleNameLst )
-        # load files and generate root
-        testIO.Init()
-        testPChecker = PresChecker(testIO.GetRoot(self.inputFilleName), testIO.GetRoot(self.outputFilleNameLst[0])  )
-        
-        # get input source list
-        sourceInputLst = GetElementsByTags(testIO.GetRoot(self.inputFilleName), self.tagLst)
-        sourceOutputLst = GetElementsByTags(testIO.GetRoot(self.outputFilleNameLst[0]), self.tagLst)
-        
-        if len(sourceInputLst) != 0 and len(sourceOutputLst) != 0:
-            
-            # get Name arrays
-            Name_arrayInput = []
-            for eachSource in sourceInputLst:
-                Name_arrayInput = GetElementsByTags(eachSource, ['Name_array'])
-                if len(Name_arrayInput) != 0:
-                    break
-            
-            Name_arrayOutput = []
-            for eachSource in sourceOutputLst:
-                Name_arrayOutput = GetElementsByTags(eachSource, ['Name_array'])
-                if len(Name_arrayOutput) != 0:
-                    break
-            
-            if len(Name_arrayInput) != 0 and len(Name_arrayOutput) != 0:
-                resSetEle = testPChecker.ResetElements(Name_arrayInput, Name_arrayOutput)
-                
-                # check whether there is interpolaton retrived correctly
-                if resSetEle[0] == True:            
-                    resChkVale = testPChecker.checkValue(True)                    
-                    if resChkVale[0] == True:                        
-                        testIO.Delink()
-                        return [True, 'Pass: interpolations in Name array are preserved.']
-                    else:
-                        testIO.Delink()
-                        return [False, resChkVale[1]]
-                else:
-                    testIO.Delink()
-                    return [False, resSetEle[1]]
-            else:
-                testIO.Delink()
-                return [False, 'Failed: Name array of import/export doesn\'t not existed.']
-        else:
-            testIO.Delink()
-            return [False, 'Failed: source is not found or number of animation is not correct.']
+    def __init__(self, _tagLst, _attrName, _attrVal, _data):
+        self.tagList = _tagLst
+        self.attrName = _attrName
+        self.attrVal = _attrVal
+        self.dataToCheck = _data
+        self.status_baseline = False
+        self.status_superior = False
+        self.status_exemplary = False
+        self.__assistant = JudgeAssistant.JudgeAssistant()
 
-    # This function is enough to test whether baked matrix is correct or not. through image comparison
     def JudgeBaseline(self, context):
+        # No step should crash
+        self.__assistant.CheckCrashes(context)
         
-        if len(self.tagLst) == 0:
-            context.Log("Error: judging script doesn't have enough information about tag names.")
-            return False
+        # Import/export/validate must exist and pass, while Render must only exist.
+        self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], ["Render"])
         
-        # This is where you can test XML or force the comparison of image files
-        # or any custom verification you want to do...
-        if (context.HasStepCrashed()):
-            context.Log("FAILED: Crashes during required steps.")
-            return False
-        else:
-            context.Log("PASSED: No crashes.")
-
-        # Check the required steps for positive results and that a rendering was done.
-        if not context.HaveStepsPassed([ "Import", "Export", "Validate" ]):
-            context.Log("FAILED: Import, export and validate steps must be present and successful.")
-            self.status_basic = 0
-            return False
+        self.status_baseline = self.__assistant.GetResults()
+        return self.status_baseline
         
-        if not context.DoesStepsExists([ "Render" ]):
-            context.Log("FAILED: A render step is required.")
-            return False
-        
-        # Get the input file
-        self.inputFilleName = context.GetAbsInputFilename(context.GetCurrentTestId())
-        
-        # Get the output file
-        outputFilenames = context.GetStepOutputFilenames("Export")
-        if len(outputFilenames) == 0:
-            context.Log("FAILED: There are no export steps.")
-            return False
-        else:
-            del self.outputFilleNameLst[:]
-            self.outputFilleNameLst.extend( outputFilenames )
-            
-        context.Log("PASSED: Required steps executed and passed.")
-        
-        # Running function for Exemplary badge
-        resNameCheck = self.NameArrayChecking()
-        
-        # record message:
-        self.msgB2M = resNameCheck[1]
-        
-        if resNameCheck[0] == True:
-            self.status_basic = 1
-            return True  
-        else:
-            self.status_basic = 0
-            context.Log("FAILED: No baseline badge defined and failed in test of Exemplary bagde.")
-            return False
-        
-    # To pass intermediate you need to pass basic, this object could also include additional 
-    # tests that were specific to the intermediate badge.
-    def JudgeExemplary(self, context):        
-        context.Log(self.msgB2M)
-        if self.status_basic == 1:
-            return True
-        else:
-            return False
-    
-    # To pass advanced you need to pass intermediate, this object could also include additional
-    # tests that were specific to the advanced badge
+    # To pass superior you need to pass baseline, this object could also include additional 
+    # tests that were specific to the superior badge.
     def JudgeSuperior(self, context):
-        context.Log("N/A")
-        return False
-
-    # To pass FX you need to pass basic?
-    # This object could also include additional
-    # tests that were specific to the FX badges
-    def JudgeFx(self, context):
-        context.Log("N/A")
-        return False
-
-    # To pass advanced you need to pass intermediate, this object could also include additional
-    # tests that were specific to the advanced badge
-    def JudgePhysics(self, context):
-        context.Log("N/A")
-        return False
-
+        # if baseline fails, no point in further checking
+        if (self.status_baseline == False):
+            self.status_superior = self.status_baseline
+            return self.status_superior
+        
+        # Checks that images show animation, then compare the import and export rendered images
+        # Lastly, check for preservation of interpolation data
+        if ( self.__assistant.HasAnimatedImages(context) ):
+            if (self.__assistant.CompareRenderedImages(context)):
+                self.__assistant.ElementDataPreserved(context, self.tagList, dataType="STRING")
+        
+        self.status_superior = self.__assistant.DeferJudgement(context)
+        return self.status_superior
+            
+    # To pass exemplary you need to pass superior, this object could also include additional
+    # tests that were specific to the exemplary badge
+    def JudgeExemplary(self, context):
+        self.status_exemplary = self.status_superior
+        return self.status_exemplary 
+       
 # This is where all the work occurs: "judgingObject" is an absolutely necessary token.
 # The dynamic loader looks very specifically for a class instance named "judgingObject".
 #
-judgingObject = SimpleJudgingObject(-1000, -1000, -1000, tagLst);
+judgingObject = SimpleJudgingObject(tagLst, attrName, attrVal, dataToCheck);
