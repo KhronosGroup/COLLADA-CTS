dd081cf94550ebc80765733280fd0b65772ec6cd
tswong <tswong@example.com>
2009-09-10 23:19:39 +0000

    Tony Wong: Committing changes and additions to library_effects test cases

--- StandardDataSets/collada/library_effects/effect/newparam/surface/type/2d/2d.py
+++ StandardDataSets/collada/library_effects/effect/newparam/surface/type/2d/2d.py
@@ -4,54 +4,68 @@
 
 # See Core.Logic.FJudgementContext for the information
 # of the 'context' parameter.
-# [WARNING] this structure is subject to changes.
-#
 
 # This sample judging object does the following:
 #
 # JudgeBaseline: just verifies that the standard steps did not crash.
-# JudgeExemplary: also verifies that the validation steps are not in error.
-# JudgeSuperior: same as intermediate badge.
+# JudgeSuperior: also verifies that the validation steps are not in error.
+# JudgeExemplary: same as intermediate badge.
+
+# We import an assistant script that includes the common verifications
+# methods. The assistant buffers its checks, so that running them again
+# does not incurs an unnecessary performance hint.
+from StandardDataSets.scripts import JudgeAssistant
+
+# Please feed your node list here:
+tagLst = ['library_effects', 'effect', 'newparam', 'surface']
+attrName = "type"
+attrVal = "2D"
+dataToCheck = ''
 
 class SimpleJudgingObject:
-    def __init__(self):
-        pass
+    def __init__(self, _tagLst, _attrName, _attrVal, _data):
+        self.tagList = _tagLst
+        self.attrName = _attrName
+        self.attrVal = _attrVal
+        self.dataToCheck = _data
+        self.status_baseline = False
+        self.status_superior = False
+        self.status_exemplary = False
+        self.__assistant = JudgeAssistant.JudgeAssistant()
         
     def JudgeBaseline(self, context):
-            
-        # This is where you can test XML or force the comparison of image files
-        # or any custom verification you want to do...
-        if (context.HasStepCrashed()):
-            context.Log("FAILED: Crashes during required steps.")
-            return False
-        else:
-            context.Log("PASSED: No crashes.")
+        # No step should not crash
+        self.__assistant.CheckCrashes(context)
         
-        # Check the required steps for positive results.
-        if not context.HaveStepsPassed([ "Import", "Export", "Validate" ]):
-            context.Log("FAILED: Import, export and validate steps must be present and successful.")
-            return False
+        # Import/export/validate must exist and pass, while Render must only exist.
+        self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], ["Render"])
 
-        # Check that a rendering has been done.
-        if not context.DoesStepsExists([ "Render" ]):
-            context.Log("FAILED: A render step is required.")
+        if (self.__assistant.GetResults() == False): 
+            self.status_baseline = False
             return False
+            
+        # Compare the rendered images
+        self.__assistant.CompareRenderedImages(context)
         
-        context.Log("PASSED: Required steps executed and passed.")
-        return True
-      
+        # Check for preservation of element
+        self.__assistant.AttributeCheck(context, self.tagList, self.attrName, self.attrVal)
+        
+        self.status_baseline = self.__assistant.DeferJudgement(context)
+        return self.status_baseline
+  
     # To pass intermediate you need to pass basic, this object could also include additional 
     # tests that were specific to the intermediate badge.
-    # QUESTION: Is there a way to fetch the result of JudgeBaseline so we don't have to run it again?
-    def JudgeExemplary(self, context):
-        return self.JudgeBaseline(context)
+    def JudgeSuperior(self, context):
+        self.status_superior = self.status_baseline
+        return self.status_superior 
             
     # To pass advanced you need to pass intermediate, this object could also include additional
     # tests that were specific to the advanced badge
-    def JudgeSuperior(self, context):
-        return self.JudgeExemplary(context)
+    def JudgeExemplary(self, context):
+        self.status_exemplary = self.status_superior
+        return self.status_exemplary 
        
 # This is where all the work occurs: "judgingObject" is an absolutely necessary token.
 # The dynamic loader looks very specifically for a class instance named "judgingObject".
 #
-judgingObject = SimpleJudgingObject();
+judgingObject = SimpleJudgingObject(tagLst, attrName, attrVal, dataToCheck);
---
+++ StandardDataSets/collada/library_effects/effect/profile_COMMON/technique/blinn/transparency/effect_blinn_transparency_param/effect_blinn_transparency_param.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2007 Khronos Group
+# Available only to Khronos members.
+# Distribution of this file or its content is strictly prohibited.
+
+# See Core.Logic.FJudgementContext for the information
+# of the 'context' parameter.
+
+# This sample judging object does the following:
+#
+# JudgeBaseline: just verifies that the standard steps did not crash.
+# JudgeSuperior: also verifies that the validation steps are not in error.
+# JudgeExemplary: same as intermediate badge.
+
+# We import an assistant script that includes the common verifications
+# methods. The assistant buffers its checks, so that running them again
+# does not incurs an unnecessary performance hint.
+from StandardDataSets.scripts import JudgeAssistant
+
+# Please feed your node list here:
+originalLocation =['library_effects', 'effect', 'profile_COMMON', 'newparam', 'float']
+bakedLocation = ['library_effects', 'effect', 'profile_COMMON', 'technique', 'blinn', 'transparency', 'float']
+newparamLocations = [['library_effects', 'effect', 'newparam', 'float'], ['library_effects', 'effect', 'profile_COMMON', 'newparam', 'float']]
+
+class SimpleJudgingObject:
+    def __init__(self, _originalLocation, _bakedLocation, _newparamLocations):
+        self.originalLocation = _originalLocation
+        self.bakedLocation = _bakedLocation
+        self.newparamLocations = _newparamLocations
+        self.status_baseline = False
+        self.status_superior = False
+        self.status_exemplary = False
+        self.__assistant = JudgeAssistant.JudgeAssistant()
+        
+    def JudgeBaseline(self, context):
+        # No step should not crash
+        self.__assistant.CheckCrashes(context)
+        
+        # Import/export/validate must exist and pass, while Render must only exist.
+        self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], ["Render"])
+
+        self.status_baseline = self.__assistant.GetResults()
+        return self.status_baseline
+  
+    # To pass intermediate you need to pass basic, this object could also include additional 
+    # tests that were specific to the intermediate badge.
+    def JudgeSuperior(self, context):
+        # if baseline fails, no point in further checking
+        if (self.status_baseline == False):
+            self.status_superior = self.status_baseline
+            return self.status_superior
+
+        # Compare the rendered images between import and export
+        # Then compare images against reference test
+        # Last, check for preservation of element data
+        if ( self.__assistant.CompareRenderedImages(context) ):
+            if ( self.__assistant.CompareImagesAgainst(context, "effect_blinn_transparency_float") ):
+                self.__assistant.NewparamCheck(context, originalLocation, bakedLocation, newparamLocations)
+        
+        self.status_superior = self.__assistant.DeferJudgement(context)
+        return self.status_superior 
+        
+    # To pass advanced you need to pass intermediate, this object could also include additional
+    # tests that were specific to the advanced badge
+    def JudgeExemplary(self, context):
+        self.status_exemplary = self.status_superior
+        return self.status_exemplary 
+       
+# This is where all the work occurs: "judgingObject" is an absolutely necessary token.
+# The dynamic loader looks very specifically for a class instance named "judgingObject".
+#
+judgingObject = SimpleJudgingObject(originalLocation, bakedLocation, newparamLocations);
