979202be5a945977755d97f87a53a71b59e9f772
tswong <tswong@example.com>
2009-10-15 06:48:35 +0000

    Added and updated exemplary test cases in library_geometries

--- StandardDataSets/collada/library_geometries/geometry/mesh/customized/4d/4d.py
+++ StandardDataSets/collada/library_geometries/geometry/mesh/customized/4d/4d.py
@@ -1,15 +1,17 @@
-# Copyright (C) 2007 Khronos Group
+# Copyright (C) 2007 - 2009 Khronos Group
 # Available only to Khronos members.
 # Distribution of this file or its content is strictly prohibited.
 
 # See Core.Logic.FJudgementContext for the information
 # of the 'context' parameter.
+#
 
 # This sample judging object does the following:
 #
-# JudgeBaseline: just verifies that the standard steps did not crash.
-# JudgeSuperior: also verifies that the validation steps are not in error.
-# JudgeExemplary: same as intermediate badge.
+# JudgeBaseline: verifies that app did not crash, the required steps have been performed, 
+#  the rendered images match, and the required element(s) has been preserved
+# JudgeExemplary: returns Baseline status.
+# JudgeSuperior: returns Baseline status.
 
 # We import an assistant script that includes the common verifications
 # methods. The assistant buffers its checks, so that running them again
@@ -17,14 +19,16 @@
 from StandardDataSets.scripts import JudgeAssistant
 
 # Please feed your node list here:
-tagLst = []
-attrName = ''
-attrVal = ''
-dataToCheck = ''
+tagLst = ['library_geometries', 'geometry', 'mesh', 'float_array']
+primTagLstArray = [['library_geometries', 'geometry', 'mesh', 'polylist', 'input'], ['library_geometries', 'geometry', 'mesh', 'triangles', 'input'], ['library_geometries', 'geometry', 'mesh', 'polygons', 'input']]
+attrName = 'semantic'
+attrVal = 'CUSTOM'
+dataToCheck = '0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15'
 
 class SimpleJudgingObject:
-    def __init__(self, _tagLst, _attrName, _attrVal, _data):
+    def __init__(self, _tagLst, _primTagLstArray, _attrName, _attrVal, _data):
         self.tagList = _tagLst
+        self.primTagListArray = _primTagLstArray
         self.attrName = _attrName
         self.attrVal = _attrVal
         self.dataToCheck = _data
@@ -39,32 +43,38 @@ class SimpleJudgingObject:
         
         # Import/export/validate must exist and pass, while Render must only exist.
         self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], ["Render"])
-       
+
         self.status_baseline = self.__assistant.GetResults()
         return self.status_baseline
   
     # To pass intermediate you need to pass basic, this object could also include additional 
     # tests that were specific to the intermediate badge.
     def JudgeSuperior(self, context):
-	# if baseline fails, no point in further checking
-        if (self.status_baseline == False):
-            self.status_superior = self.status_baseline
-            return self.status_superior
-
-        # Compare the rendered images between import and export
-        # Then compare images against reference test to check for non-equivalence
-        if ( self.__assistant.CompareRenderedImages(context) ):
-            self.__assistant.CompareImagesAgainst(context, "_reference_position_texcoord_color")
-        
-        self.status_superior = self.__assistant.DeferJudgement(context)
+        self.status_superior = self.status_baseline
         return self.status_superior 
             
     # To pass advanced you need to pass intermediate, this object could also include additional
     # tests that were specific to the advanced badge
     def JudgeExemplary(self, context):
-        self.status_exemplary = self.status_superior
+	# if superior fails, no point in further checking
+        if (self.status_superior == False):
+            self.status_exemplary = self.status_superior
+            return self.status_exemplary
+
+        # Check for preservation of element data and then semantic attribute
+        if ( self.__assistant.ElementDataCheck(context, self.tagList, dataToCheck, "float") ):
+            attributePreserved = False
+            for eachPrimTagList in self.primTagListArray:
+                if ( self.__assistant.AttributeCheck(context, eachPrimTagList, self.attrName, self.attrVal) ):
+                    attributePreserved = True
+                    break
+                    
+            self.status_exemplary = attributePreserved
+        else:
+            self.status_exemplary = self.__assistant.DeferJudgement(context)
+
         return self.status_exemplary 
-       
+        
 # This is where all the work occurs: "judgingObject" is an absolutely necessary token.
 # The dynamic loader looks very specifically for a class instance named "judgingObject".
 #
--- StandardDataSets/collada/library_geometries/geometry/mesh/vertices/input/position_texcoord_color_normal/position_texcoord_color_normal.py
+++ StandardDataSets/collada/library_geometries/geometry/mesh/vertices/input/position_texcoord_color_normal/position_texcoord_color_normal.py
@@ -1,15 +1,17 @@
-# Copyright (C) 2007 Khronos Group
+# Copyright (C) 2007 - 2009 Khronos Group
 # Available only to Khronos members.
 # Distribution of this file or its content is strictly prohibited.
 
 # See Core.Logic.FJudgementContext for the information
 # of the 'context' parameter.
+#
 
 # This sample judging object does the following:
 #
-# JudgeBaseline: just verifies that the standard steps did not crash.
-# JudgeSuperior: also verifies that the validation steps are not in error.
-# JudgeExemplary: same as intermediate badge.
+# JudgeBaseline: verifies that app did not crash, the required steps have been performed, 
+#  the rendered images match, and the required element(s) has been preserved
+# JudgeExemplary: returns Baseline status.
+# JudgeSuperior: returns Baseline status.
 
 # We import an assistant script that includes the common verifications
 # methods. The assistant buffers its checks, so that running them again
@@ -39,32 +41,32 @@ class SimpleJudgingObject:
         
         # Import/export/validate must exist and pass, while Render must only exist.
         self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], ["Render"])
-       
+
         self.status_baseline = self.__assistant.GetResults()
         return self.status_baseline
   
     # To pass intermediate you need to pass basic, this object could also include additional 
     # tests that were specific to the intermediate badge.
     def JudgeSuperior(self, context):
-	# if baseline fails, no point in further checking
-        if (self.status_baseline == False):
-            self.status_superior = self.status_baseline
-            return self.status_superior
-
-        # Compare the rendered images between import and export
-        # Then compare images against reference test to check for non-equivalence
-        if ( self.__assistant.CompareRenderedImages(context) ):
-            self.__assistant.CompareImagesAgainst(context, "_reference_position_texcoord_color")
-        
-        self.status_superior = self.__assistant.DeferJudgement(context)
+        self.status_superior = self.status_baseline
         return self.status_superior 
             
     # To pass advanced you need to pass intermediate, this object could also include additional
     # tests that were specific to the advanced badge
     def JudgeExemplary(self, context):
-        self.status_exemplary = self.status_superior
+	# if superior fails, no point in further checking
+        if (self.status_superior == False):
+            self.status_exemplary = self.status_superior
+            return self.status_exemplary
+
+        # Compare the rendered images between import and export
+        # Then compare images against reference test for non equivalence
+        if ( self.__assistant.CompareRenderedImages(context) ):
+            self.__assistant.CompareImagesAgainst(context, "position_texcoord_color", None, None, 5, True, False)
+        
+        self.status_exemplary = self.__assistant.DeferJudgement(context)
         return self.status_exemplary 
-       
+        
 # This is where all the work occurs: "judgingObject" is an absolutely necessary token.
 # The dynamic loader looks very specifically for a class instance named "judgingObject".
 #
