03c979b80b3951036fb64a23676a985ac3587b04
giles <giles@example.com>
2010-01-10 19:28:13 +0000

    Alan: initial HowToAddSoftware docs

---
+++ Documentation/HowToAddSoftware/Scripts/FDebug.py
@@ -0,0 +1,110 @@
+# Copyright (C) 2006 Khronos Group
+# Available only to Khronos members.
+# Distribution of this file or its content is strictly prohibited.
+
+import os.path
+
+import Core.Common.FUtils as FUtils
+from Core.Logic.FSettingEntry import *
+from Scripts.FApplication import *
+
+class FDebug (FApplication):
+    """The class which represents mimic to the testing framework.
+    
+    """
+    
+    __SCRIPT_EXTENSION = ".py"
+    
+    def __init__(self, configDict):
+        """__init__() -> FDebug"""
+        FApplication.__init__(self, configDict)
+        self.__script = None
+        self.__currentFilename = None
+        self.__currentImageName = None
+        self.__currentImportProperName = None
+        self.__testImportCount = 0
+        self.__testRenderCount = 0
+        self.__workingDir = None
+    
+    def GetPrettyName(self):
+        """GetPrettyName() -> str
+        
+        Implements FApplication.GetPrettyName()
+        
+        """
+        return "Debug 1.0"
+    
+    def GetSettingsForOperation(self, operation):
+        """GetSettingsForOperation(operation) -> list_of_FSettingEntry
+        
+        Implements FApplication.GetSettingsForOperation()
+        
+        """
+        if (operation == IMPORT):
+            return []
+        elif (operation == EXPORT):
+            return []
+        elif (operation == RENDER): 
+            return []
+        else:
+            return []
+    
+    def BeginScript(self, workingDir):
+        """BeginScript(workingDir) -> None
+        
+        Implements FApplication.BeginScript()
+        
+        """
+        pyFilename = ("script" + str(self.applicationIndex) + 
+                FDebug.__SCRIPT_EXTENSION)
+        self.__script = open(os.path.join(workingDir, pyFilename), "w")
+        self.WriteCrashDetectBegin(self.__script)
+        
+        self.__testImportCount = 0
+        self.__testRenderCount = 0
+        self.__workingDir = workingDir
+    
+    def EndScript(self):
+        """EndScript() -> None
+        
+        Implements FApplication.EndScript()
+        
+        """
+        self.__script.close()
+    
+    def RunScript(self):
+        """RunScript() -> None
+        
+        Implements FApplication.RunScript()
+        
+        """
+        returnValue = 0
+                
+        return (returnValue == 0)
+    
+    def WriteImport(self, filename, logname, outputDir, settings, isAnimated):
+        """WriteImport(filename, logname, outputDir, settings, isAnimated) -> list_of_str
+                
+        """
+        print "Write Import filename: %s  logname: %s  outputDir: %s" % (filename, logname, outputDir)        
+        return ["foo.dae"]
+        
+    
+    def WriteRender(self, logname, outputDir, settings, isAnimated):
+        """WriteRender(logname, outputDir, settings, isAnimated) -> list_of_str
+        
+        Implements FApplication.WriteRender()
+        
+        """
+        print "Render logname: %s  outputDir: %s" % (logname, outputDir)        
+        return ["foo.dae"]
+
+    
+    def WriteExport(self, logname, outputDir, settings, isAnimated):
+        """WriteImport(logname, outputDir, settings, isAnimated) -> list_of_str
+        
+        Implements FApplication.WriteExport()
+        
+        """
+        print "Write Export logname: %s  outputDir: %s" % (logname, outputDir)        
+        return ["foo.dae"]
---
+++ Documentation/HowToAddSoftware/TestIntegration/mimic_render.bat
@@ -0,0 +1,4 @@
+echo "mimic %1 %2 %3"
+
+copy %1 %2
+copy c:\ctf\TestIntegration\black.png %3
