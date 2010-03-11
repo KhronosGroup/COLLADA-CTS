3f2024907615fc133ce1ac67328a1e42645eedf1
giles <giles@example.com>
2010-03-11 02:32:15 +0000

    Alan: updated for 0.9.1 release

--- Release/buildctszip.sh
+++ Release/buildctszip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SPEC_VERSION=1.4
-CTS_REVISION=0.9.0
-RELEASE_DATE=20100110
+CTS_REVISION=0.9.1
+RELEASE_DATE=20100310
 
 cd ..
 find . -type f -print | grep -v ".svn" | zip COLLADA-${SPEC_VERSION}-CTS-${CTS_REVISION}-${RELEASE_DATE}.zip -@
