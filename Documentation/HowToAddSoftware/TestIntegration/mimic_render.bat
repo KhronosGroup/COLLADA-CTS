c60414f1d70ec8d4a7086fefe8817ad0e167b0e8
giles <giles@example.com>
2011-02-15 16:11:08 +0000

    Alan: made path relative

--- Documentation/HowToAddSoftware/TestIntegration/mimic_render.bat
+++ Documentation/HowToAddSoftware/TestIntegration/mimic_render.bat
@@ -1,4 +1,5 @@
 echo "mimic %1 %2 %3"
 
 copy %1 %2
-copy c:\ctf\Documentation\HowToAddSoftware\TestIntegration\black.png %3
+#copy c:\ctf\Documentation\HowToAddSoftware\TestIntegration\black.png %3
+copy ..\Documentation\HowToAddSoftware\TestIntegration\black.png %3
