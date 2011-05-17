df572a5a521e9307b9560299d09693955f4aba63
tswong <tswong@example.com>
2011-05-17 00:54:12 +0000

    Update library_formulas test cases.

--- StandardDataSets/1_5/collada/library_formulas/formula/multi_formula/multi_formula.py
+++ StandardDataSets/1_5/collada/library_formulas/formula/multi_formula/multi_formula.py
@@ -18,71 +18,143 @@ from StandardDataSets.scripts import JudgeAssistant
 
 # Please feed your node list here:
 
-tagLstNewparam   = ['library_formulas', 'formula', 'newparam']
-newparamAtttName = 'sid'
-tagLstTarget = ['library_formulas', 'formula', 'target', 'param']
+tagLstRoot             = ['library_formulas', 'formula']
+rootAttrName           = 'id'
+tagLstNewparam         = ['library_formulas', 'formula', 'newparam']
+newparamAtttName       = 'sid'
+tagLstTarget           = ['library_formulas', 'formula', 'target', 'param']
+tagLstTechnique        = ['library_formulas', 'formula', 'technique_common']
+tagLstNewParamDataType = [['newparam', 'float'], ['newparam', 'int']]
 
 # formula 1
-newparamAttrVal1  = ['target1', 'value', 'pitch']
-tagLstCsymbol1 = [['library_formulas', 'formula', 'technique_common', 'math:math', 'math:apply', 'math:apply', 'math:csymbol'], 
-		  ['library_formulas', 'formula', 'technique_common', 'math:math', 'math:apply', 'math:csymbol']]
+rootAttrVal1           = 'pythagorean'
+newparamAttrVal1       = ['hypotenuse', 'side1', 'side2']
+tagLstCsymbol1         = ['library_formulas', 'formula', 'technique_common', 'math', 'apply', 'apply', 'apply', 'csymbol']
+newparamCount1         = [1, 2]
 
 # formula 2
-newparamAttrVal2  = ['hypotenuse', 'side1', 'side2']
-tagLstCsymbol2 = ['library_formulas', 'formula', 'technique_common', 'math:math', 'math:apply', 'math:apply', 'math:csymbol']
+rootAttrVal2           = 'pitch'
+newparamAttrVal2       = ['target1', 'value', 'pitch']
+tagLstCsymbol2         = [['library_formulas', 'formula', 'technique_common', 'math', 'apply', 'apply', 'csymbol'], 
+		          ['library_formulas', 'formula', 'technique_common', 'math', 'apply', 'csymbol']]
+newparamCount2         = [3, 0]
 
 class SimpleJudgingObject:
-    def __init__(self, _tagLstNewparam, _newparamAtttName, _tagLstTarget, _newparamAttrVal1, _tagLstCsymbol1, _newparamAttrVal2, _tagLstCsymbol2):
-        self.tagLstNewparam = _tagLstNewparam
+    def __init__(self, _tagLstRoot, _rootAttrName, _tagLstNewparam, _newparamAtttName, _tagLstTarget, _tagLstTechnique, _tagLstNewParamDataType,
+                 _rootAttrVal1, _newparamAttrVal1, _tagLstCsymbol1, _newparamCount1,
+                 _rootAttrVal2, _newparamAttrVal2, _tagLstCsymbol2, _newparamCount2):
+                 
+        self.tagListRoot = _tagLstRoot
+        self.rootAttrName = _rootAttrName
+        self.tagListNewparam = _tagLstNewparam
         self.newparamAtttName = _newparamAtttName
-        self.tagLstTarget = _tagLstTarget
+        self.tagListTarget = _tagLstTarget
+        self.tagListTechnique = _tagLstTechnique
+        self.tagListNewparamDataType = _tagLstNewParamDataType
         
+        self.rootAttrVal1 = _rootAttrVal1
         self.newparamAttrVal1 = _newparamAttrVal1
-        self.tagLstCsymbol1 = _tagLstCsymbol1
+        self.tagListCsymbol1 = _tagLstCsymbol1
+        self.newparamCount1 = _newparamCount1
         
+        self.rootAttrVal2 = _rootAttrVal2
         self.newparamAttrVal2 = _newparamAttrVal2
-        self.tagLstCsymbol2 = _tagLstCsymbol2
+        self.tagListCsymbol2 = _tagLstCsymbol2
+        self.newparamCount2 = _newparamCount2
         
         self.status_baseline = False
         self.status_superior = False
         self.status_exemplary = False
         self.__assistant = JudgeAssistant.JudgeAssistant()
+
+    # Add the name space to the tags
+    def AddNSToTagList(self, context):
+        nameSpace = self.__assistant.GetNameSpace(context, self.tagListTechnique)
         
+       	if (nameSpace != None):
+       	    for i in range(3, len(self.tagListCsymbol1)):
+       	        self.tagListCsymbol1[i] = nameSpace + ":" + self.tagListCsymbol1[i]
+       	    
+       	    for i in range(3, len(self.tagListCsymbol2[0])):
+       	        self.tagListCsymbol2[0][i] = nameSpace + ":" + self.tagListCsymbol2[0][i]
+       	    
+       	    for i in range(3, len(self.tagListCsymbol2[1])):
+       	        self.tagListCsymbol2[1][i] = nameSpace + ":" + self.tagListCsymbol2[1][i]
+       	        
+    def CheckFormula1(self, context):
+
+        # check that the newparam attributes are preserved
+        if ( (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal1[0])) or
+             (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal1[1])) or
+             (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal1[2])) ):
+            return False
+        
+        # check that the target data is preserved
+        if ( not self.__assistant.ElementDataCheck(context, self.tagListTarget, self.newparamAttrVal1[0], "string") ):
+            return False
+        
+        # check that the newparam data types are preserved
+        if ( (self.__assistant.GetElementCount(self.tagListRoot, self.rootAttrName, self.rootAttrVal1, self.tagListNewparamDataType[0]) != self.newparamCount1[0]) ):
+            context.Log("FAILED: newparam <float> type not preserved")
+            return False
+        else:
+            context.Log("PASSED: newparam <float> type is preserved")
+        
+        if ( (self.__assistant.GetElementCount(self.tagListRoot, self.rootAttrName, self.rootAttrVal1, self.tagListNewparamDataType[1]) != self.newparamCount1[1]) ):
+            context.Log("FAILED: newparam <int> type not preserved")
+            return False
+        else:
+            context.Log("PASSED: newparam <int> type is preserved")
+        
+        # check that the csymbol data is preserved
+        if ( (not self.__assistant.ElementDataCheck(context, self.tagListCsymbol1, self.newparamAttrVal1[1], "string")) or
+             (not self.__assistant.ElementDataCheck(context, self.tagListCsymbol1, self.newparamAttrVal1[2], "string")) ):
+            return False
+        
+        return True
+
+    def CheckFormula2(self, context):
+
+        # check that the newparam attributes are preserved
+        if ( (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal2[0])) or
+             (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal2[1])) or
+             (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal2[2])) ):
+            return False
+        
+        # check that the target data is preserved
+        if ( not self.__assistant.ElementDataCheck(context, self.tagListTarget, self.newparamAttrVal2[0], "string") ):
+            return False
+        
+        # check that the newparam data types are preserved
+        if ( (self.__assistant.GetElementCount(self.tagListRoot, self.rootAttrName, self.rootAttrVal2, self.tagListNewparamDataType[0]) != self.newparamCount2[0]) ):
+            context.Log("FAILED: newparam <float> type not preserved")
+            return False
+        else:
+            context.Log("PASSED: newparam <float> type is preserved")
+        
+        if ( (self.__assistant.GetElementCount(self.tagListRoot, self.rootAttrName, self.rootAttrVal2, self.tagListNewparamDataType[1]) != self.newparamCount2[1]) ):
+            context.Log("FAILED: newparam <int> type not preserved")
+            return False
+        else:
+            context.Log("PASSED: newparam <int> type is preserved")
+        
+        # check that the csymbol data is preserved
+        if ( (not self.__assistant.ElementDataCheck(context, self.tagListCsymbol2[0], self.newparamAttrVal2[1], "string")) or
+             (not self.__assistant.ElementDataCheck(context, self.tagListCsymbol2[1], self.newparamAttrVal2[2], "string")) ):
+            return False
+        
+        return True
+
+
     def JudgeBaseline(self, context):
         # No step should not crash
         self.__assistant.CheckCrashes(context)
         
         # Import/export/validate must exist and pass, while Render must only exist.
-        self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], ["Render"])
+        self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], [])
         
         self.status_baseline = self.__assistant.GetResults()
         return self.status_baseline
-
-    def CheckFormula1(self):
-        # check that the newparam attributes are preserved
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal1[0])
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal1[1])
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal1[2])
-        
-        # check that the target data is preserved
-        self.__assistant.ElementDataCheck(context, self.tagLstTarget, self.newparamAttrVal1[0], "string")
-        
-        # check that the csymbol data is preserved
-        self.__assistant.ElementDataCheck(context, self.tagLstCsymbol1[0], self.newparamAttrVal1[1], "string")
-        self.__assistant.ElementDataCheck(context, self.tagLstCsymbol1[1], self.newparamAttrVal1[2], "string")
-        
-    def CheckFormula2(self):
-        # check that the newparam attributes are preserved
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal2[0])
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal2[1])
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal2[2])
-        
-        # check that the target data is preserved
-        self.__assistant.ElementDataCheck(context, self.tagLstTarget, self.newparamAttrVal2[0], "string")
-        
-        # check that the csymbol data is preserved
-        self.__assistant.ElementDataCheck(context, self.tagLstCsymbol2, self.newparamAttrVal2[1], "string")
-        self.__assistant.ElementDataCheck(context, self.tagLstCsymbol2, self.newparamAttrVal2[2], "string")
 
     # To pass intermediate you need to pass basic, this object could also include additional 
     # tests that were specific to the intermediate badge.
@@ -98,18 +170,18 @@ class SimpleJudgingObject:
             self.status_exemplary = self.status_superior
             return self.status_exemplary
         
-        self.CheckFormula1()
+        self.AddNSToTagList(context)
         
-        if (self.__assistant.GetResults() == False):
+        if (self.CheckFormula1(context) == False):
 	    self.status_exemplary = False
             return self.status_exemplary 
         
-        self.CheckFormula2()
-        
-        self.status_exemplary = self.__assistant.DeferJudgement(context)
+        self.status_exemplary = self.CheckFormula2(context)
         return self.status_exemplary 
        
 # This is where all the work occurs: "judgingObject" is an absolutely necessary token.
 # The dynamic loader looks very specifically for a class instance named "judgingObject".
 #
-judgingObject = SimpleJudgingObject(tagLstNewparam, newparamAtttName, tagLstTarget, newparamAttrVal1, tagLstCsymbol1, newparamAttrVal2, tagLstCsymbol2);
+judgingObject = SimpleJudgingObject(tagLstRoot, rootAttrName, tagLstNewparam, newparamAtttName, tagLstTarget, tagLstTechnique, tagLstNewParamDataType,
+                                    rootAttrVal1, newparamAttrVal1, tagLstCsymbol1, newparamCount1,
+                                    rootAttrVal2, newparamAttrVal2, tagLstCsymbol2, newparamCount2);
--- StandardDataSets/1_5/collada/library_formulas/formula/one_formula/one_formula.py
+++ StandardDataSets/1_5/collada/library_formulas/formula/one_formula/one_formula.py
@@ -17,34 +17,88 @@
 from StandardDataSets.scripts import JudgeAssistant
 
 # Please feed your node list here:
-tagLstNewparam   = ['library_formulas', 'formula', 'newparam']
-newparamAtttName = 'sid'
-newparamAttrVal  = ['hypotenuse', 'side1', 'side2']
+tagLstNewparam         = ['library_formulas', 'formula', 'newparam']
+newparamAtttName       = 'sid'
+newparamAttrVal        = ['hypotenuse', 'side1', 'side2']
+tagLstTarget           = ['library_formulas', 'formula', 'target', 'param']
+tagLstTechnique        = ['library_formulas', 'formula', 'technique_common']
+tagLstCsymbol          = ['library_formulas', 'formula', 'technique_common', 'math', 'apply', 'apply', 'apply', 'csymbol']
 
-tagLstTarget = ['library_formulas', 'formula', 'target', 'param']
-
-tagLstCsymbol = ['library_formulas', 'formula', 'technique_common', 'math:math', 'math:apply', 'math:apply', 'math:csymbol']
+tagLstRoot             = ['library_formulas', 'formula']
+rootAttrName           = 'id'
+rootAttrVal            = 'pythagorean'
+tagLstNewParamDataType = [['newparam', 'float'], ['newparam', 'int']]
+newparamCount          = [1, 2]
 
 class SimpleJudgingObject:
-    def __init__(self, _tagLstNewparam, _newparamAtttName, _newparamAttrVal, _tagLstTarget, _tagLstCsymbol):
-        self.tagLstNewparam = _tagLstNewparam
+    def __init__(self, _tagLstNewparam, _newparamAtttName, _newparamAttrVal, _tagLstTarget, _tagLstTechnique, _tagLstCsymbol,
+                 _tagLstRoot, _rootAttrName, _rootAttrVal, _tagLstNewParamDataType, _newparamCount):
+                 
+        self.tagListNewparam = _tagLstNewparam
         self.newparamAtttName = _newparamAtttName
         self.newparamAttrVal = _newparamAttrVal
+        self.tagListTarget = _tagLstTarget
+        self.tagListTechnique = _tagLstTechnique
+        self.tagListCsymbol = _tagLstCsymbol
         
-        self.tagLstTarget = _tagLstTarget
-        self.tagLstCsymbol = _tagLstCsymbol
+        self.tagListRoot = _tagLstRoot
+        self.rootAttrName = _rootAttrName
+        self.rootAttrVal = _rootAttrVal
+        self.tagListNewparamDataType = _tagLstNewParamDataType
+        self.newparamCount = _newparamCount
         
         self.status_baseline = False
         self.status_superior = False
         self.status_exemplary = False
         self.__assistant = JudgeAssistant.JudgeAssistant()
+
+    # Add the name space to the tags
+    def AddNSToTagList(self, context):
+        nameSpace = self.__assistant.GetNameSpace(context, self.tagListTechnique)
+        
+       	if (nameSpace != None):
+       	    for i in range(3, len(self.tagListCsymbol)):
+       	        self.tagListCsymbol[i] = nameSpace + ":" + self.tagListCsymbol[i]
+
+    def CheckFormula(self, context):
+        self.AddNSToTagList(context)
+    
+        # check that the newparam attributes are preserved
+        if ( (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal[0])) or
+             (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal[1])) or
+             (not self.__assistant.AttributeCheck(context, self.tagListNewparam, self.newparamAtttName, self.newparamAttrVal[2])) ):
+            return False
+        
+        # check that the target data is preserved
+        if ( not self.__assistant.ElementDataCheck(context, self.tagListTarget, self.newparamAttrVal[0], "string") ):
+            return False
+        
+        # check that the newparam data types are preserved
+        if ( (self.__assistant.GetElementCount(self.tagListRoot, self.rootAttrName, self.rootAttrVal, self.tagListNewparamDataType[0]) != self.newparamCount[0]) ):
+            context.Log("FAILED: newparam <float> type not preserved")
+            return False
+        else:
+            context.Log("PASSED: newparam <float> type is preserved")
+        
+        if ( (self.__assistant.GetElementCount(self.tagListRoot, self.rootAttrName, self.rootAttrVal, self.tagListNewparamDataType[1]) != self.newparamCount[1]) ):
+            context.Log("FAILED: newparam <int> type not preserved")
+            return False
+        else:
+            context.Log("PASSED: newparam <int> type is preserved")
+        
+        # check that the csymbol data is preserved
+        if ( (not self.__assistant.ElementDataCheck(context, self.tagListCsymbol, self.newparamAttrVal[1], "string")) or
+             (not self.__assistant.ElementDataCheck(context, self.tagListCsymbol, self.newparamAttrVal[2], "string")) ):
+            return False
+        
+        return True
         
     def JudgeBaseline(self, context):
         # No step should not crash
         self.__assistant.CheckCrashes(context)
         
         # Import/export/validate must exist and pass, while Render must only exist.
-        self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], ["Render"])
+        self.__assistant.CheckSteps(context, ["Import", "Export", "Validate"], [])
         
         self.status_baseline = self.__assistant.GetResults()
         return self.status_baseline
@@ -63,22 +117,11 @@ class SimpleJudgingObject:
             self.status_exemplary = self.status_superior
             return self.status_exemplary
         
-        # check that the newparam attributes are preserved
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal[0])
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal[1])
-        self.__assistant.AttributeCheck(context, self.tagLstNewparam, self.newparamAtttName, self.newparamAttrVal[2])
-        
-        # check that the target data is preserved
-        self.__assistant.ElementDataCheck(context, self.tagLstTarget, self.newparamAttrVal[0], "string")
-        
-        # check that the csymbol data is preserved
-        self.__assistant.ElementDataCheck(context, self.tagLstCsymbol, self.newparamAttrVal[1], "string")
-        self.__assistant.ElementDataCheck(context, self.tagLstCsymbol, self.newparamAttrVal[2], "string")
-
-        self.status_exemplary = self.__assistant.DeferJudgement(context)
+        self.status_exemplary = self.CheckFormula(context)
         return self.status_exemplary 
        
 # This is where all the work occurs: "judgingObject" is an absolutely necessary token.
 # The dynamic loader looks very specifically for a class instance named "judgingObject".
 #
-judgingObject = SimpleJudgingObject(tagLstNewparam, newparamAtttName, newparamAttrVal, tagLstTarget, tagLstCsymbol);
+judgingObject = SimpleJudgingObject(tagLstNewparam, newparamAtttName, newparamAttrVal, tagLstTarget, tagLstTechnique, tagLstCsymbol,
+                                    tagLstRoot, rootAttrName, rootAttrVal, tagLstNewParamDataType, newparamCount);
